a58216c6291e738e1c4ab58095bceaabbb92dbc7
Rachel Radford <rachelradford21@gmail.com>
2022-07-06 13:29:14 -0400

    Initial Commit

---
+++ FEMTimeTrackingDashboard/FEMTimeTrackingDashboardApp.swift
@@ -0,0 +1,17 @@
+//
+//  FEMTimeTrackingDashboardApp.swift
+//  FEMTimeTrackingDashboard
+//
+//  Created by Rachel Radford on 7/6/22.
+//
+
+import SwiftUI
+
+@main
+struct FEMTimeTrackingDashboardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
